90e1a745e489fd76526a82698acdd520536697fa
bobejaan10toon <141512571+bobejaan10toon@users.noreply.github.com>
2024-01-09 14:23:31 +0200

    Translate and modify comments in remap.lua

--- lua/miscellaneous/remap.lua
+++ lua/miscellaneous/remap.lua
@@ -19,10 +19,10 @@ vim.keymap.set("n", "<leader>Y", "\"+y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- Hierdie word gedoen omdat daar blykbaar 'n klein verskil tussen die twee is
+-- This is done because apparently there is a small difference
 vim.keymap.set("i", "j<C-c>", "<Esc>")
 
-vim.keymap.set("n", "Q", "<nop>") -- Apparently Q is the worst place in the universe
+vim.keymap.set("n", "Q", "<nop>") -- Apparently Q is the worst place in the universe???
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -32,7 +32,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Ek het geen idee hoekom niemand hierdie doen nie, dit maak net so veel sin
 vim.keymap.set("i", "<C-k>", "<Up>")
 vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-l>", "<Right>")
